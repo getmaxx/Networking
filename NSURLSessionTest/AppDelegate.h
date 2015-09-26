ed08de7d8e4ae85ee0126959303ece5485817134
–Ш–≥–Њ—А—М –Т–µ–і–µ–љ–µ–µ–≤ <igorv@mac-mini-igor.local>
2015-09-26 19:32:34 +0300

    Initial Commit

---
+++ NSURLSessionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionTest
+//
+//  Created by Игорь Веденеев on 26.09.15.
+//  Copyright © 2015 Игорь Веденеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSessionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSessionTest
+//
+//  Created by Игорь Веденеев on 26.09.15.
+//  Copyright © 2015 Игорь Веденеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
